8a01815388de917f1397101f10fc57b2d128a488
VeduSah <vedantsahani6@gmail.com>
2025-05-23 13:30:05 +0530

    dd

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const connectDB = require('./src/config/db');
+const serverless = require('serverless-http');
 
+dotenv.config();
+
+const connectDB = require('./src/config/db');
 const CustomerRoutes = require('./src/routes/CustomerRoutes');
 const ServiceRoutes = require('./src/routes/ServiceRoutes');
 const StaffRoutes = require('./src/routes/StaffRoutes');
@@ -12,19 +15,16 @@ const ProductRoutes = require('./src/routes/ProductRoutes');
 const NewEntry = require('./src/routes/NewentryRoutes');
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
-dotenv.config();
-connectDB();
 
-const PORT = process.env.PORT || 5000;
+connectDB(); // make sure this avoids reconnecting on every request
 
-// 👇 Add this route for root
 app.get('/', (req, res) => {
   res.send('✅ Your site is live and working!');
 });
 
-// Register API routes
 app.use('/customer', CustomerRoutes);
 app.use('/service', ServiceRoutes);
 app.use('/staff', StaffRoutes);
@@ -32,6 +32,7 @@ app.use('/custdirt', CustomerdRoutes);
 app.use('/product', ProductRoutes);
 app.use('/entry', NewEntry);
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server started on port ${PORT}`);
-});
+// Do NOT call app.listen(...)
+// Instead export it:
+module.exports = app;
+module.exports.handler = serverless(app);
